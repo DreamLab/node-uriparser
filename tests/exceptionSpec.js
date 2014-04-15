056751bc096d4438801789ec58b4f51d8d49d682
Jakub Lekstan <kuebzky@gmail.com>
2014-04-15 10:52:28 +0200

    Fixed tests

--- tests/exceptionSpec.js
+++ tests/exceptionSpec.js
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-var uriparser = require("../lib/bindings.js");
+var uriparser = require("../bin/uriparser.node");
 
 
 describe("uriparser - exceptions", function () {
