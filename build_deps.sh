ceb33b75a8279513121880a009a4b8f2e11b6ae0
Marcin Kaciuba <aldor007@gmail.com>
2016-04-16 15:53:20 +0200

    Fix build_deps for OS X

--- build_deps.sh
+++ build_deps.sh
@@ -1,7 +1,29 @@
 #!/bin/bash -x
 
 BUILDDIR=$(dirname $0)
-BUILDDIR="$(readlink -f $BUILDDIR)/build"
+
+# bash readlink
+# start 
+# source http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+TARGET_FILE=$BUILDDIR
+
+cd `dirname $TARGET_FILE`
+TARGET_FILE=`basename $TARGET_FILE`
+
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET_FILE" ]
+do
+TARGET_FILE=`readlink $TARGET_FILE`
+cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+done
+
+# Compute the canonicalized name by finding the physical path 
+# for the directory we're in and appending the target file.
+PHYS_DIR=`pwd -P`
+BUILDDIR="$PHYS_DIR/$TARGET_FILE/build"
+# end
+
 URIDIR="$BUILDDIR/uriparser"
 NGXDIR="$BUILDDIR/ngx_url_parser"
 
@@ -9,7 +31,7 @@ if [ ! -f $NGXDIR/lib/libngx_url_parser.a ]; then
     mkdir -p $NGXDIR  && cd deps/ngx_url_parser && ./autogen.sh && ./configure --with-pic  --disable-shared --prefix=$NGXDIR && make clean install
 fi
 
-cd -
+cd $BUILDDIR/../
 
 if [ ! -f $URIDIR/lib/liburiparser.a ]; then
     mkdir -p $URIDIR && cd deps/uriparser && ./autogen.sh && ./configure --with-pic --disable-test --disable-doc --disable-shared --prefix=$URIDIR && make clean install
